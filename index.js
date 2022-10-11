0fe2896b2a82623a19a0d0f5ab02de8ebbaa77fd
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-11 17:24:20 -0400

    commit tree fcb808d6ba062a0db48d126ea0326557dc53dc79

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(startingBlock){
+    if (startingBlock > 42) {
+        return startingBlock - 42;
+    } 
+    else if (startingBlock < 42){
+        return 42 - startingBlock;
+    }
+}
+
+function distanceFromHqInFeet(startingBlock){
+    let blockDistance = distanceFromHqInBlocks(startingBlock);   
+    return (blockDistance * 264)
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start > end) {
+        return ((start * 264) - (end * 264));
+    } 
+    else if (start < end){
+        return ((end * 264) - (start * 264));
+    }
+}
+
+function calculatesFarePrice(start, end){
+    // since I am used previously generated numbers simply use a let
+    // statement to call the information from that function
+    let distance = distanceTravelledInFeet(start, end);
+    if (distance < 400) return (0)
+    if (distance > 400 && distance < 2000) return ((distance - 400) * .02)
+    if (distance > 2000 && distance < 2500) return (25)
+    if (distance >= 2500) return ('cannot travel that far')
+}
